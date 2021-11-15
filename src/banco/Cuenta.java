e27b95968a7d1e0b6a3cbe87704fe10ab2949da8
gabir <gabir@LAPTOP-3QLE8S9H.technicolor.net>
2021-11-15 20:08:19 +0100

    Solo métodos vacíos

---
+++ src/banco/Cuenta.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package banco;
+
+/**
+ *
+ * @author gabir
+ */
+public class Cuenta {
+    private int saldo;
+    private String nCuenta;
+    
+    public Cuenta(int dineroIni, String cuenta){
+        
+    }
+    
+    public void retirar(int dinero){
+        
+    }
+    
+    public void ingresar(int dinero){
+        
+    }
+    
+    public int getSaldo(){
+        return 0;
+    }
+    
+    public String getCuenta(){
+        return "0";
+    }
+}
